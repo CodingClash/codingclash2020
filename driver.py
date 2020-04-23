6b3c4734fb9a53f193fd1350e6683c5452433f27
Ayush Rautwar <arautwar@gmail.com>
2020-04-22 22:21:31 -0400

    Add foundation

---
+++ driver.py
@@ -0,0 +1,6 @@
+import sys
+
+board_size = 30
+board = [[None]*board_size]*board_size
+team1 = sys.argv[1]
+team2 = sys.argv[2]
---
+++ example_bot/bot.py
@@ -0,0 +1,9 @@
+class gunner:
+	def __init__(self, team_in):
+		self.team = team_in
+
+class tank:
+	def __init__(self, team_in):
+		self.team = team_in
+
+

26c9a2d60cb5afc81dcde7c8a500228ce265b19e
Srikar Gouru <srikarg89@gmail.com>
2020-04-24 01:04:50 -0400

    Working version

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from engine.supervisor import Supervisor
 
-game = Supervisor("example/bot.py", "example/bot.py")
+game = Supervisor("example/example1/bot.py", "example/example1/bot.py")
+#game = Supervisor("example/example1/bot.py", "example/example2/bot.py")
 game.run_turn()
-print("HAI")
-game.run_turn()
+game.run()

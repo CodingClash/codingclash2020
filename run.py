0cd4bf73be0f2ff5bfe68b4777bb3e8fe2504de1
Srikar Gouru <srikarg89@gmail.com>
2020-04-24 20:13:40 -0400

    Enable visualizer

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ from engine.visualizer.visualizer import Visualizer
 game = Supervisor("example/ayush/bot.py", "example/gunsblazin/bot.py")
 #game = Supervisor("example/example1/bot.py", "example/example2/bot.py")
 
-#vis = Visualizer()
-#game.run_visualized(vis, max_rounds=200, delay=0.2)
-game.run(max_rounds=200)
+vis = Visualizer()
+game.run_visualized(vis, max_rounds=200, delay=0.2)
+#game.run(max_rounds=200)

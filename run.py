278b238396b55b31ef15b6fffded724281e22883
Srikar Gouru <srikarg89@gmail.com>
2020-04-24 20:11:58 -0400

    gunsblazin

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from engine.supervisor import Supervisor
 from engine.visualizer.visualizer import Visualizer
 
-game = Supervisor("example/ayush/bot.py", "example/ayush/bot.py")
+game = Supervisor("example/ayush/bot.py", "example/gunsblazin/bot.py")
 #game = Supervisor("example/example1/bot.py", "example/example2/bot.py")
 
-vis = Visualizer()
-game.run_visualized(vis, max_rounds=200, delay=0.2)
-#game.run()
+#vis = Visualizer()
+#game.run_visualized(vis, max_rounds=200, delay=0.2)
+game.run(max_rounds=200)

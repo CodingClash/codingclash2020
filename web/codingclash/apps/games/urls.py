37ed0373392690473b872bffef3e6c8141a7e2cd
Srikar Gouru <srikarg89@gmail.com>
2020-05-16 18:59:50 -0400

    Create all the navbar tabs

---
+++ web/codingclash/apps/games/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.shortcuts import render
+
+from . import views
+
+app_name = "games"
+
+
+def default(request):
+    return render(request, f"games/{request.resolver_match.url_name}.html")
+
+
+urlpatterns = [
+    path("about/", default, name="about"),
+    path("rules/", default, name="rules"),
+    path("schedule/", default, name="schedule"),
+]

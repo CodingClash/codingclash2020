d313d4ec934ae2e74f2a7d601832c8bccca7b2ac
Srikar Gouru <srikarg89@gmail.com>
2020-05-16 21:18:08 -0400

    Leaderboard search bar

--- web/codingclash/static/js/games/leaderboard.js
+++ web/codingclash/static/js/games/leaderboard.js
@@ -1,30 +1,42 @@
 const leaderboard_data = [
-    ["Bot 1", 100],
-    ["Bot 2", 90],
-    ["Bot 3", 80],
-    ["Bot 4", 70],
-    ["Bot 5", 60],
-    ["Bot 6", 50],
-    ["Bot 7", 40],
-    ["Bot 8", 30],
-    ["Bot 9", 20],
-    ["Bot 10", 10],
-    ["Bot 11", 0],
-    ["Bot 12", -10],
-    ["Bot 13", -20],
-    ["Bot 14", -30],
-    ["Bot 15", -40],
-    ["Bot 16", -50],
-    ["Bot 17", -60],
-    ["Bot 18", -70],
-    ["Bot 19", -80],
-    ["Bot 20", -90],
+    [1, "Bot 1", 100],
+    [2, "Bot 2", 90],
+    [3, "Bot 3", 80],
+    [4, "Bot 4", 70],
+    [5, "Bot 5", 60],
+    [6, "Bot 6", 50],
+    [7, "Bot 7", 40],
+    [8, "Bot 8", 30],
+    [9, "Bot 9", 20],
+    [10, "Bot 10", 10],
+    [11, "Bot 11", 0],
+    [12, "Bot 12", -10],
+    [13, "Bot 13", -20],
+    [14, "Bot 14", -30],
+    [15, "Bot 15", -40],
+    [16, "Bot 16", -50],
+    [17, "Bot 17", -60],
+    [18, "Bot 18", -70],
+    [19, "Bot 19", -80],
+    [20, "Bot 20", -90],
 ];
 
 const NUM_PER_BLOCK = 10;
 
 // Assumes data is sorted
 // Split data into blocks (a block is what is displayed per page)
+
+function search(){
+    let searchTerm = $("#searchBox").val();
+    let matches = [];
+    for(let i = 0; i < leaderboard_data.length; i++){
+        if(leaderboard_data[i][1].indexOf(searchTerm) != -1){
+            matches.push(leaderboard_data[i]);
+        }
+    }
+    set_table(matches, 0);
+    return false;
+}
 
 function set_buttons(data, block_num){
     let num_blocks = Math.ceil(data.length / NUM_PER_BLOCK);
@@ -47,13 +59,13 @@ function get_row(rank, username, score){
     return row;
 }
 
-function set_table(block, start_num){
-    console.log(block);
+function set_table(blocks){
+    console.log(blocks);
     let table = $("#table");
     table.empty();
     table.append(get_row("Rank", "Team name", "ELO rating"));
-    for(let i = 0; i < block.length; i++){
-        table.append(get_row(start_num + i + 1, block[i][0], block[i][1]));
+    for(let i = 0; i < blocks.length; i++){
+        table.append(get_row(blocks[i][0], blocks[i][1], blocks[i][2]));
     }
 }
 
@@ -64,7 +76,7 @@ function display(data, block_num){
     for(let i = min; i < Math.min(data.length, max); i++){
         block.push(data[i]);
     }
-    set_table(block, min);
+    set_table(block);
 }
 
 function set_leaderboard(block_num){

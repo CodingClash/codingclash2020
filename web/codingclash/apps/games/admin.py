079c82e8ada51e20d800d964296a03974e17bf74
Srikar Gouru <srikarg89@gmail.com>
2020-07-28 17:07:52 -0400

    Remove website from engine repo

--- web/codingclash/apps/games/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Game)
-admin.site.register(Submission)
--- web/run_celery.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#celery worker -A codingclash -l info
-celery -A codingclash worker -B -E -l info

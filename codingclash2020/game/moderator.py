b81f4e2bf82ea05b731003e14c7959ba2e6dc9f4
Srikar Gouru <srikarg89@gmail.com>
2020-09-15 09:27:10 -0400

    Fix grenader attacking error

--- codingclash2020/game/moderator.py
+++ codingclash2020/game/moderator.py
@@ -216,18 +216,18 @@ class Moderator:
         squares = squares_within_distance(robot.attack_aoe)
         for dx, dy in squares:
             loc = (target_location[0] + dx, target_location[1] + dy)
+            if not self.inbounds(loc):
+                continue
             target_robot = self.get_robot(loc)
-            worked = True
             if target_robot == RobotType.NONE:
-                worked = False
+                continue
             if target_robot.team.color == robot.team.color:
-                worked = False
+                continue
             # worked = True
             # for i in self.sense(robot):
             #     if i.team != robot.team.color and in_between(robot.location, loc, i.location):
             #         worked = False
-            if worked:
-                target_robots.append(target_robot)
+            target_robots.append(target_robot)
         filtered, reason = robot.can_attack(target_robots)
         if reason:
             raise Exception(reason)
